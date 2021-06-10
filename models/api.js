b9bb9c719693f51f401002d192d9095b94b9842d
alan <alan.coiffar@gmail.com>
2021-06-10 10:24:33 +0200

    select date fonctionne avec la map + mise en place cercle

--- models/api.js
+++ models/api.js
@@ -3,39 +3,16 @@ console.log(environment);
 const configuration   = require('../knexfile')[environment];      // extraire la base de données correcte avec les configurations d'environnement
 const database        = require('knex')(configuration);
 
-// const selectId = function(request, response){
-//   console.log("Entre dans afficher animal");
-//   const idSelect = request.body.listeAnimaux
-//   console.log("idSelect : ", idSelect);
-//   request.session.idSelect = idSelect;
-//   _findAnimalById(idSelect)
-//     .then(foundAnimals => {
-//       animaux = foundAnimals.rows
-//       console.log("animal : ", animaux);
-//       return animaux
-//     })
-//     .then(() => {
-//       request.session.animal = animaux;
-//     })
-//     .catch((err) => console.error(err))
-//   response.redirect("/")
-// }
-//
-// const _findAnimalById = (id_animal) => {
-//   let request = database.raw("SELECT * FROM animaux WHERE id_animal = ?", [id_animal], (err, res) => {
-//     //console.log(err ? err.stack : "test ",res.rows[0]) // Hello World!
-//     database.end()
-//   })
-//   return request
-// }
-
 const afficherAnimal = (request, response) => {
   console.log("Entre dans afficher animal");
   const idSelect = request.body.listeAnimaux
   console.log(idSelect);
   request.session.idAnimalSelect = idSelect;
 
-  findGPS(idSelect)
+  let dateDebut = request.cookies.dateDebut;
+  let dateFin = request.cookies.dateFin;
+
+  findGPS(idSelect, dateDebut, dateFin)
   .then(foundGPS => {
     request.session.gps = foundGPS;
     response.redirect('/home');
@@ -43,12 +20,8 @@ const afficherAnimal = (request, response) => {
 
 }
 
-const stockerGPS = (foundGPS) => {
-  request.session.gps = data;
-}
-
-const findGPS = (idAnimal) => {
-  return database.raw("SELECT * FROM gps WHERE id_Animal = ?", [idAnimal])
+const findGPS = (idAnimal, dateDebut, dateFin) => {
+  return database.raw("SELECT * FROM gps WHERE id_Animal = ? AND date BETWEEN ? AND ?", [idAnimal, dateDebut, dateFin])
   .then((data) => {
     return data.rows
   });
